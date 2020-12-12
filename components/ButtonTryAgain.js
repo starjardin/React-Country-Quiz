10debd54fc64fe02886353dc3f6cd0f41c0966c9
Tantely Andrianarivola <tantely.and@onja.org>
2020-12-12 14:09:42 +0300

    added styles to the wrong answers

--- components/ButtonTryAgain.js
+++ components/ButtonTryAgain.js
@@ -18,7 +18,7 @@ export default function ButtonNext () {
         <h3 className="results">Results</h3>
         <p>You got <span>{score}</span> correct answers</p>
         <p>
-          {score != 0 && `Here is your reward ${score} ${score > 1 ? ": slices of pizzas" : ": slice of pizza"} 🍕`} 
+          {score != 0 && `Here is your reward : ${score} ${score > 1 ? " slices of pizzas" : "slice of pizza"} 🍕`} 
         </p>
         <p>
           {score > 5 && score <= 10 && "Good score . Why don't you try again, you'll get heigher score"}
